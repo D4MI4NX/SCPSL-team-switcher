ecd417a4f7a38d1bdd0cff1588e4f4bae1f084f2
D4MI4NX <105593627+D4MI4NX@users.noreply.github.com>
2024-11-24 15:01:43 +0100

    Initial commit

---
+++ EventHandler.cs
@@ -0,0 +1,17 @@
+namespace TeamSwitcher
+{
+    public class GenHandler(TeamSwitcher pluginInstance)
+    {
+        private readonly TeamSwitcher ex = pluginInstance;
+
+        public void Start()
+        {
+            
+        }
+
+        public void Stop()
+        {
+            
+        }
+    }
+}
---
+++ TeamSwitcher.cs
@@ -0,0 +1,35 @@
+﻿using Exiled.API.Features;
+
+namespace TeamSwitcher
+{
+    public class TeamSwitcher : Plugin<Config.Config>
+    {
+        public override string Name => "Team Switcher";
+    
+        public override string Prefix => "team_switcher";
+        
+        public override string Author => "D4MI4NX";
+        
+        public override Version Version => new(0, 1, 0);
+        
+        public override Version RequiredExiledVersion => new(8, 9, 11);
+
+
+        public GenHandler Handler { get; private set; }
+
+        public override void OnEnabled()
+        {
+            Handler = new GenHandler(this);
+            Handler.Start();
+
+            base.OnEnabled();
+        }
+        
+        public override void OnDisabled()
+        {
+            Handler?.Stop();
+
+            base.OnDisabled();
+        }
+    }
+}
